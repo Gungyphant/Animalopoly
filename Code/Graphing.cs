ea3b4941a60d9615d9b83749cb17210ee01b1c24
Gungyphant <gungyphant@gmail.com>
2025-05-26 11:37:27 +0100

    Added horizontal lines, axes limits, and increased resolution to 4k

--- Code/Graphing.cs
+++ Code/Graphing.cs
@@ -34,21 +34,40 @@ namespace Animalopoly.Code
             {
                 Directory.CreateDirectory(filepathForImage[..filepathForImage.LastIndexOf('/')]);
                 ScottPlot.Plot graph = new();
+                int maxX = 0;
+                int maxY = 0;
+                int minY = 0;
                 foreach (Player player in points.Keys)
                 {
                     List<int> xData = new List<int>();
                     List<int> yData = new List<int>();
                     foreach (Tuple<int, int> point in points[player])
                     {
-                        xData.Add(point.Item1);
-                        yData.Add(point.Item2);
+                        int x = point.Item1;
+                        int y = point.Item2;
+                        if (x > maxX) maxX = x;
+                        if (y > maxY) maxY = y;
+                        if (y < minY) minY = y;
+                        xData.Add(x);
+                        yData.Add(y);
                     }
-                    var plot = graph.Add.Scatter(xData, yData);
-                    plot.LegendText = Convert.ToString(player.getName());
-                    plot.Color = ConsoleColorToScottPlotColour[colours[player.getId()]];
+                    Scatter plot = graph.Add.Scatter(xData, yData);
+                    plot.LegendText = Convert.ToString(player.GetName());
+                    plot.Color = ConsoleColorToScottPlotColour[colours[player.GetId()]];
+                    plot.LineWidth = 5;
+                }
+                // Generate dashed horizontal lines
+                foreach ((int y, string name) in new Tuple<int, string>[2] { new Tuple<int, string>(0, "Bankrupt"), new Tuple<int, string>(3750, "Starting money") })
+                {
+                    int[] yData = Enumerable.Repeat(y, maxX).ToArray();
+                    Signal line = graph.Add.Signal(yData);
+                    line.LegendText = name;
+                    line.LinePattern = LinePattern.DenselyDashed;
+                    line.LineWidth = 5;
                 }
                 graph.ShowLegend();
-                graph.SavePng(filepathForImage, 400, 300);
+                graph.Axes.SetLimits(0, maxX, minY, maxY);
+                graph.SavePng(filepathForImage, 3840, 2160); // 4k
             }
         }
     }
