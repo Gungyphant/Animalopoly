95650f4d4b288670bd8dbfb632c6f419e6ab2fd3
Gungyphant <gungyphant@gmail.com>
2025-09-10 16:21:02 +0100

    Added Writes to inform the user when graph generation is taking place

--- Code/Program.cs
+++ Code/Program.cs
@@ -138,7 +138,9 @@ namespace Animalopoly.Code
                 winner = recentlyBankrupted[Array.IndexOf(recentlyBankruptedMoneys, recentlyBankruptedMoneys.Max())];
             }
             WriteLine($"[{colourNames[winner.GetId()]}]Player {winner.GetName()}[white] wins with £{winner.GetMoney()}!");
+            Write("Generating money graph...");
             grapher.GenerateGraph($"../../../Save files/{currentGameName}/Money graph.png");
+            WriteLine($"{new string('\b', 100)}Money graph saved to Save files/{currentGameName}/Money graph.png");
         }
     }
 }
