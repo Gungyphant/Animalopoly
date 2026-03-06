78839d94ddce81bf02fc2a81da2dd63ab36c9e1a
Gungyphant <gungyphant@gmail.com>
2026-03-06 12:14:49 +0000

    Implemented Hard AI

--- Code/AI.cs
+++ Code/AI.cs
@@ -1,4 +1,5 @@
-﻿using static Animalopoly.Code.CardClass;
+﻿using System.ComponentModel;
+using static Animalopoly.Code.CardClass;
 using static Animalopoly.Code.CommandLineInterface;
 using static Animalopoly.Code.Commands;
 using static Animalopoly.Code.Graphing;
@@ -23,7 +24,7 @@ namespace Animalopoly.Code
             int mostExpensiveStopCost = 0;
             foreach (Tile tile in locations)
             {
-                if (tile is Animal animal_to_check && animal_to_check.GetStopCost() > mostExpensiveStopCost)
+                if (tile is Animal animal_to_check && animal_to_check.GetOwner() is not null && animal_to_check.GetStopCost() > mostExpensiveStopCost)
                 {
                     mostExpensiveStopCost = animal_to_check.GetStopCost();
                 }
@@ -40,10 +41,34 @@ namespace Animalopoly.Code
 
             return (player.GetMoney() - animal.GetBuyCost() - mostExpensiveStopCost - mostCostlyCardCost) > 0;
         }
+        private static double HARD_TURNTHRESHOLD = 20; // Median value
         public static bool Hard(string question, Animal animal, Player player)
         {
             // Buy/upgrade if the charge/cost is above a certain threshold, and Medium
-            return false;
+            if (!Medium(question, animal, player))
+            {
+                return false;
+            }
+            int cost = animal.GetBuyCost();
+            int gainPerStop;
+            if (question == "buy")
+            {
+                gainPerStop = animal.GetStopCost();
+            }
+            else if (question == "upgrade")
+            {
+                gainPerStop = animal.GetNextStopCost() - animal.GetStopCost();
+            }
+            else
+            {
+                throw new Exception(question);
+            }
+            double landsToEarnBack = (double)cost / gainPerStop;
+            // When there are P players, there are (P - 1) other players. If it is assumed they are in random positions around the board, each of them
+            //  has a 1/26 chance of landing on this property on their turn, so there are an expected (P - 1)/26 lands per turn
+            double landsPerTurn = (players.Length - 1) / 26.0;
+            double turnsToEarnBack = landsToEarnBack / landsPerTurn;
+            return (turnsToEarnBack <= HARD_TURNTHRESHOLD);
         }
         public static bool Expert(string question, Animal animal, Player player)
         {
